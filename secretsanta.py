80b94e4ea108a8bdea40d00f1668fc29db520145
dicaijudicar <57874494+dicaijudicar@users.noreply.github.com>
2020-11-08 21:50:18 -0500

    Update secretsanta.py
    updated with better logic, mutliple logic tests related to secret santa allocation with possibility for additions

--- secretsanta.py
+++ secretsanta.py
@@ -6,64 +6,64 @@
 
 import random
 import pandas as pd
-import sys
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+import numpy as np
 
 
-L=['1','Julien','2','3','4','5','6']
-L
-df=pd.DataFrame(L)
-df=df.rename(columns={0:"Name"})
-df['SecretSanta']=0
-
-z=[0,1,2,3,4,5,6]
-random.shuffle(z)
-for i in range(0,7):
-    df['SecretSanta'].loc[i]=L[z[i]]
-
-
-#testing
-for i in range(0,7):
-    if df.Name.loc[i]==df['SecretSanta'].loc[i]:
-        sys.exit("Name match")
-    else:
-        print('All good')
         
 
 df2=pd.read_excel("E:\\Programs\\Python\\emails.xlsx")
-#df2=pd.DataFrame(emails).rename(columns={0:"Email"})
-df3=pd.DataFrame(['Julien','etc']).rename(columns={0:"Name"})
-df4=pd.concat([df2,df3],axis=1); del df2,df3
-#creat finalized frame
-final=pd.merge(df,df4, on='Name')
 
-'''
-#testing
-for email in emails:
-    dfred=final[final.Email==email]
-    print('')
-    print(dfred.iloc[0]['Name'])
-    print(email)
-    print(dfred.iloc[0]['SecretSanta'])
-'''
-ParentsWhoShareEmail='NameOfParents'
+
+list_ind=list(df2['Name'].unique())
+df2['SecretSanta']=''
+
+z=[]
+[z.append(i) for i in range(0,len(list_ind))]
+
+        
+
+def shuffleAttach(df):
+    random.shuffle(z)
+    for row,rowS in df2.iterrows():
+        df2.loc[row,'SecretSanta']=list_ind[z[row]]
+    return df2
+
+
+twoTruth=True
+oneTruth=True
+while oneTruth==True or twoTruth==True:
+    df2=shuffleAttach(df2)
+    df2['testFilter1']=np.where(df2.Name==df2.SecretSanta,1,0)
+    oneTruth= True if df2['testFilter1'].sum() > 0 else False
+    df2['testFilter2']=np.where(df2.last_year==df2.SecretSanta,1,0)
+    twoTruth= True if df2['testFilter2'].sum()>0 else False
+
+
 #loop over emails
-for name in L:     
-    dfred=final[final.Name==name]
+for name in df2['Name'].unique():     
     import smtplib
     from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
     
-    name=dfred.iloc[0]['Name'];secretsanta=dfred.iloc[0]['SecretSanta']
+    secretsanta=df2[df2.Name==name].iloc[0]['SecretSanta']
     
-    message = f"Hello {name}. I hope you've had a swell day. Mostly, I am making a long message so {ParentsWhoShareEmail} can't read eachother's people. You are the Secret Santa for {secretsanta}."
+    message = f"Hello {name}. I wish you a good day during these Covid times. Are you ready for this? *Cue music* You are the Secret Santa for {secretsanta}."
     msg = MIMEMultipart()
-    password = "password" #insert password here 
-    msg['From'] = "juliendicaire@gmail.com" #feel free to contact me with any questions
-    msg['To'] = dfred.iloc[0]['Email']  
-    msg['Subject'] = f"This message is for {name}. If you are not {name}, please disregard! Your Secret Santa Person!" # Type the subject of your message 
+    password = pd.read_excel(r"password").iloc[0,0]
+    msg['From'] = "juliendicaire@gmail.com"
+    msg['To'] = df2[df2.Name==name].iloc[0]['Email']  
+    msg['Subject'] = f"This message is for {name}. If you are not {name}, please disregard! Your Secret Santa Person awaits you!"
     msg.attach(MIMEText(message, 'plain'))
     server = smtplib.SMTP('smtp.gmail.com: 587')
     server.starttls()
     server.login(msg['From'], password)
     server.sendmail(msg['From'], msg['To'], msg.as_string())
     server.quit()
+    
+    
+    
+#export to excel the details 
+df2.to_excel("E:\\Programs\\Python\\emails.xlsx", index=False)
